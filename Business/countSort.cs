d4015cd690ca60fe344c2c7c8f4b144ece3730b8
Anton Zudov <antony.zudov@mail.ru>
2018-09-08 20:03:03 +0300

    The Full Counting Sort

---
+++ Business/countSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    [Description("https://www.hackerrank.com/challenges/countingsort4/problem")]
+    public static class countSort
+    {
+        public static string StableCountingSort(List<List<string>> arr)
+        {
+            var halfLength = (int) arr.Count / 2;
+
+            for (var i = 0; i < halfLength; i++)
+            {
+                arr[i][1] = "-";
+            }
+
+            const int k = 10;
+
+            var b = new List<List<string>>();
+            foreach (var ai in arr)
+            {
+                b.Add(new List<string>());
+            }
+
+            var c = new List<int>();
+            for (var i = 0; i < k; i++)
+            {
+                c.Add(0);
+            }
+
+            foreach (var ai in arr)
+            {
+                c[Convert.ToInt16(ai[0])] += 1;
+            }
+
+            for (var j = 1; j < k; j++)
+            {
+                c[j] += c[j - 1];
+            }
+
+            for (var i = arr.Count - 1; i >= 0; i--)
+            {
+                var ind = Convert.ToInt16(arr[i][0]);
+                c[ind] -= 1;
+                b[c[ind]] = arr[i];
+            }
+
+            return string.Join(" ", b.Select(x => x[1]).ToList());
+        }
+    }
+}
--- Tests/SortingTests.cs
+++ Tests/SortingTests.cs
@@ -120,5 +120,38 @@ namespace Tests
 
             Assert.Equal(3, actual);
         }
+
+        [Fact]
+        [Description("https://www.hackerrank.com/challenges/countingsort4/problem")]
+        public void countSort_Test()
+        {
+            var arr = new List<List<string>>
+            {
+                new List<string> {"0", "abc"},
+                new List<string> {"6", "cd"},
+                new List<string> {"0", "ef"},
+                new List<string> {"6", "gh"},
+                new List<string> {"4", "ij"},
+                new List<string> {"0", "ab"},
+                new List<string> {"6", "cd"},
+                new List<string> {"0", "ef"},
+                new List<string> {"6", "gh"},
+                new List<string> {"0", "ij"},
+                new List<string> {"4", "that"},
+                new List<string> {"3", "be"},
+                new List<string> {"0", "to"},
+                new List<string> {"1", "be"},
+                new List<string> {"5", "question"},
+                new List<string> {"1", "or"},
+                new List<string> {"2", "not"},
+                new List<string> {"4", "is"},
+                new List<string> {"2", "to"},
+                new List<string> {"4", "the"},
+            };
+
+            var actual = countSort.StableCountingSort(arr);
+
+            Assert.Equal("- - - - - to be or not to be - that is the question - - - -", actual);
+        }
     }
 }

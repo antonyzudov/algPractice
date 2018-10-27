422d6a052ae9febd6b9670c1c0e1e2851a2088b7
Anton Zudov <antony.zudov@mail.ru>
2018-10-27 18:44:33 +0300

    Left Rotation

---
+++ Business/Arrays/ArrayLeftRotation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Business.Arrays
+{
+    [Description("https://www.hackerrank.com/challenges/array-left-rotation/problem")]
+    public static class ArrayLeftRotation
+    {
+        public static List<int> LeftRotate(IList<int> list, int d)
+        {
+            if (list?.Any() != true)
+            {
+                return new List<int>();
+            }
+
+            var n = list.Count;
+
+            var result = new List<int>();
+            for (var i = 0; i < n; i++)
+            {
+                result.Add(0);
+            }
+
+            for (var i = 0; i < n; i++)
+            {
+                var index = GetIndex(i, d, n);
+
+                result[index] = list[i];
+            }
+
+            return result;
+        }
+
+        private static int GetIndex(int i, int d, int n)
+        {
+            var index = i - d;
+
+            if (index < 0)
+            {
+                index += n;
+            }
+
+            return index;
+        }
+    }
+}
--- Tests/ArraysTests.cs
+++ Tests/ArraysTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using Business;
+using Business.Arrays;
 using Xunit;
 
 namespace Tests
@@ -24,6 +25,18 @@ namespace Tests
             var actual = DynamicArray.dynamicArray(n, queries);
 
             Assert.Equal(new List<int> { 7, 3 }, actual);
+        }
+
+        [Fact]
+        [Description("https://www.hackerrank.com/challenges/array-left-rotation/problem")]
+        public void LeftRotate_Test()
+        {
+            var d = 4;
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+
+            var actual = ArrayLeftRotation.LeftRotate(list, d);
+
+            Assert.Equal(new List<int> { 5, 1, 2, 3, 4 }, actual);
         }
     }
 }

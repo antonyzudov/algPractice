1810de6f58bdd01fce7b883d0bdaafcf1d7c5087
Anton Zudov <antony.zudov@mail.ru>
2018-10-27 10:59:32 +0300

    Dynamic Array

---
+++ Business/Arrays/DynamicArray.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Business
+{
+    [Description("https://www.hackerrank.com/challenges/dynamic-array/problem")]
+    public static class DynamicArray
+    {
+        public static List<int> dynamicArray(int n, List<List<int>> queries)
+        {
+            var result = new List<int>();
+
+            var seqList = new List<List<int>>();
+
+            for( var i = 0; i < n; i++)
+            {
+                seqList.Add(new List<int>());
+            }
+
+            var lastAnswer = 0;
+
+            foreach(var query in queries)
+            {
+                var type = query[0];
+                var x = query[1];
+                var y = query[2];
+
+                if (type == 1)
+                {
+                    var index = (x ^ lastAnswer) % n;
+                    seqList[index].Add(y);
+                }
+
+                if (type == 2)
+                {
+                    var index = (x ^ lastAnswer) % n;
+
+                    var size = seqList[index].Count;
+
+                    lastAnswer = seqList[index][y % size];
+
+                    result.Add(lastAnswer);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/ArraysTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Business;
+using Xunit;
+
+namespace Tests
+{
+    public class ArraysTests
+    {
+        [Fact]
+        [Description("https://www.hackerrank.com/challenges/dynamic-array/problem")]
+        public void dynamicArray_Test()
+        {
+            var n = 2;
+            var queries = new List<List<int>>
+            {
+                new List<int> { 1, 0, 5 },
+                new List<int> { 1, 1, 7 },
+                new List<int> { 1, 0, 3 },
+                new List<int> { 2, 1, 0 },
+                new List<int> { 2, 1, 1 }
+            };
+
+            var actual = DynamicArray.dynamicArray(n, queries);
+
+            Assert.Equal(new List<int> { 7, 3 }, actual);
+        }
+    }
+}
